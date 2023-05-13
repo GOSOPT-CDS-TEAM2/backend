c9712f0ec95e5c7775bf90956b996b455f60b2c9
RInRInPark <surinp8019@naver.com>
2023-05-14 01:31:26 +0900

    [INIT] 프로젝트 초기세팅

---
+++ src/main/java/sopt/org/oliveyoungServer/OliveyoungServerApplication.java
@@ -0,0 +1,13 @@
+package sopt.org.oliveyoungServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliveyoungServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OliveyoungServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sopt/org/oliveyoungServer/OliveyoungServerApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.org.oliveyoungServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliveyoungServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
